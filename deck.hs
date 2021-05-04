7312cb48e9bb02e41c20965cad0390ba4973763e
lain4 <laithanh91@gmx.de>
2021-05-04 18:02:27 +0200

    TODO: Mau Mau rules

--- deck.hs
+++ deck.hs
@@ -1,52 +1,112 @@
 import Data.Bits
 import Data.List
 import Data.Maybe
+import Control.Monad
+import System.Random
+import System.Random.Shuffle
 
 
 colors = ["Karo", "Herz", "Pik", "Kreuz"]
-symbols = ["Joker", "Ass", "Koenig", "Dame", "Bube", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
-deck = decodeDeck
-
-printDeck = do { print " "
-               ; print "Kartendeck:"
-               ; print " "
-               ; mapM print (shuffle deck 378) --(shuffle deck)
-               ; print " " }
+symbols = ["Ass", "Koenig", "Dame", "Bube", "10", "9", "8", "7"]
+deckSize = (length colors) * (length symbols)
 
 
+data Game = Game { deck :: [Int]
+                 , hands :: [[Int]]
+                 , players :: [Int]
+                 , activePlayer :: Int
+                 } deriving (Show)
 
--- shuffle
-shuffle :: [String] -> Int -> [String] -- tar im then teil muss bereits eine liste sein
-shuffle tar n = if n > 0 then (shuffle (innerShuffle (split tar)) (n-1)) else tar
 
-split :: [String] -> ([String],[String])
-split ar = splitList ar mid
+-- TODO Deckgröße abhängig von Handgröße und Spielerzahl, MauMau-Regeln
+
+main :: IO()
+main = do 
+  putStrLn "Welcome to MauMau!"
+  putStrLn " "
+  putStrLn "How many players?"
+  num_players <- getLine
+  putStrLn " "
+  putStrLn "How many cards per hand?"
+  num_hand <- getLine
+  putStrLn " "
+  putStrLn ("Generating deck for " ++ (show num_players) ++ " players")
+  putStrLn "Shuffling deck"
+  g <- getStdGen
+  ps <- return ([0..((read num_players)-1)])
+  game <- return (Game (shuffle' buildDeck deckSize g) [[]] ps 0)
+  mapM putStrLn (decode (deck game))
+  putStrLn " "
+  putStrLn "Preparing players hands"
+  game <- return (Game (reduceDeck (read num_players) (read num_hand) (deck game)) (takeHand (read num_players) (read num_hand) (deck game) (hands game)) (players game) 0)
+  putStrLn " "
+  putStrLn " "
+  putStrLn "Starting game with 2 players:"
+  turn 0 game
+
+
+-- Make turns, take hands and cards, game loop in "turn"
+turn :: Int -> Game -> IO ()
+turn p game = do
+  putStrLn ("Turn of player number " ++ show p)
+  game <- return (takeCard p game)
+  if length (deck game) == 0 then putStrLn ("Game over, winning player is " ++ show p)
+  else turn (nextPlayer p game) game
+
+
+printHands :: Int -> Game -> IO ()
+printHands n game = do
+  mapM putStrLn (decode ((hands game) !! (n-1)))
+  putStrLn " "
+  when ((n-1) > 0) $ printHands (n-1) game
+
+
+takeCard :: Int -> Game -> Game
+takeCard p game = Game (reduceDeck 1 1 (deck game)) (takeFrom p game) (players game) p
+
+
+takeHand :: Int -> Int -> [Int] -> [[Int]] -> [[Int]]
+takeHand p n deck hands = [ take n (drop hand_length deck) ++ [] | hand_length <- [0,n..((p-1)*n)]]
+
+
+
+
+
+takeFrom :: Int -> Game -> [[Int]]
+takeFrom p game = [ if fromJust (elemIndex hand (hands game)) == p then head (take 1 (deck game)) : hand else hand | hand <- (hands game)]
+
+
+
+reduceDeck :: Int -> Int -> [Int] -> [Int]
+reduceDeck p n deck = drop (p * n) deck 
+
+
+
+nextPlayer :: Int -> Game -> Int
+nextPlayer p game
+  | p == lastPlayer = 0
+  | otherwise       = p + 1
   where
-    mid = (length deck) `div` 2
-
-innerShuffle :: ([String],[String]) -> [String]
-innerShuffle s = takeAll (fst s) (snd s)
-  where
-    takeAll :: [String] -> [String] -> [String]
-    takeAll a b = if a == [] then [] else head a : if b == [] then [] else head b : takeAll (tail a) (tail b)
-
-splitList :: [String] -> Int -> ([String],[String])
-splitList xs n = (take n xs , drop n xs )
+    lastPlayer = (length (players game)) - 1
 
 
 
--- Main functions
-decodeDeck :: [String]
-decodeDeck = [ decode x | x <- buildDeck]
 
+
+-- Deck creation and string representation
 buildDeck :: [Int]
 buildDeck = [ encode "color" color .|. encode "symbol" symbol | color <- [0..((length colors) - 1)], symbol <- [0..((length symbols) - 1)]]
 
 
 
+decode :: [Int] -> [String]
+decode deck = [ decode' x | x <- deck]
+
+
+
 -- Decoding
-decode :: Int -> String
-decode n = colors!!a ++ " " ++ symbols!!b
+decode' :: Int -> String
+decode' n = colors!!a ++ " " ++ symbols!!b
   where
     a = shiftR (n .&. 49152) 14   -- bit 15,16 to 1
     b = findSetBit (n .&. 16383)  -- first 14 bits to 1
